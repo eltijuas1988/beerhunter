c200fdedc1a16f0a5f0826d9ee5449ff86398846
Daniel Rojas <crimsondex@yahoo.com>
2016-04-05 00:57:02 -0700

    can add rating scores and messages for my pubs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322065859) do
+ActiveRecord::Schema.define(version: 20160405055158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20160322065859) do
 
   add_index "breweries", ["user_id"], name: "index_breweries_on_user_id", using: :btree
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "review"
+    t.integer  "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["brewery_id"], name: "index_reviews_on_brewery_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,10 +56,13 @@ ActiveRecord::Schema.define(version: 20160322065859) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "breweries", "users"
+  add_foreign_key "reviews", "breweries"
 end
